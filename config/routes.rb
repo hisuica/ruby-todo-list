08f5266c9ece88e4518bdbd85445a9d1eaecb254
Heesoo.Kim <jpz2384@linecorp.com>
2020-01-07 17:51:52 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+# config/routes.rb
+Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute" if
+  Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+end
