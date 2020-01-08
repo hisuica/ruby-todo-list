751d39e308c213f46fff3127b81947babe6d38ee
Heesoo.Kim <jpz2384@linecorp.com>
2020-01-08 18:09:10 +0900

    Added React Webpack And Typescript Config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute" if
-  Rails.env.development?
+  if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  root 'schedule#index'
+  post "/graphql", to: "graphql#execute"
 end
