44dc65d3d00a596258a5f864c067e2d81e131861
Heesoo.Kim <jpz2384@linecorp.com>
2020-01-09 14:15:47 +0900

    Set Apollo Client

---
+++ app/javascript/utils/apollo.ts
@@ -0,0 +1,70 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink, Observable } from "apollo-link";
+
+const cache = new InMemoryCache();
+
+// getToken from meta tags
+const getToken = () => document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+const token = getToken();
+const setTokenForOperation = async operation =>
+  operation.setContext({
+    headers: {
+      'X-CSRF-token': token
+    }
+  });
+
+// link with token
+const createLinkWithToken = () =>
+  new ApolloLink(
+    (operation, forward) =>
+      new Observable(observer => {
+        let handle;
+        Promise.resolve(operation)
+          .then(setTokenForOperation)
+          .then(() => {
+            handle = forward(operation).subscribe({
+              next: observer.next.bind(observer),
+              error: observer.error.bind(observer),
+              complete: observer.complete.bind(observer)
+            });
+          })
+          .catch(observer.error.bind(observer));
+        return () => {
+          if (handle) handle.unsubscribe();
+        };
+      })
+  );
+
+// log errors
+const logError = (error, _) => console.error(error);
+// create error link
+const createErrorLink = () => onError(({ graphQLErrors, networkError, operation }) => {
+  if (graphQLErrors) {
+    logError('GraphQL - Error', {
+      errors: graphQLErrors,
+      operationName: operation.operationName,
+      variables: operation.variables
+    });
+  }
+  if (networkError) logError('GraphQL - NetworkError', networkError);
+});
+
+// http link
+const createHttpLink = () => new HttpLink({
+  uri: '/graphql',
+  credentials: 'include'
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    createErrorLink(),
+    createLinkWithToken(),
+    createHttpLink()
+  ]),
+  cache
+});
+
+export default client
